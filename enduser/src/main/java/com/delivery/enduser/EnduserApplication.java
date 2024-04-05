8eb6cfc7fff0b165578b27e6e79fe74af91c510c
Aman <amanverma225@gmail.com>
2024-04-05 16:28:45 +0530

    Updateed

--- enduser/src/main/java/com/delivery/enduser/EnduserApplication.java
+++ enduser/src/main/java/com/delivery/enduser/EnduserApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EnduserApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EnduserApplication.class, args);
 	}
 
